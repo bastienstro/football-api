ff420b35d1a28c9cd10a8814a1cd0492cce3e0da
bastienstro <35740784+bastienstro@users.noreply.github.com>
2018-01-24 21:19:36 +0100

    Update PlayersController.php

--- src/ApiBundle/Controller/PlayersController.php
+++ src/ApiBundle/Controller/PlayersController.php
@@ -23,21 +23,20 @@ use ApiBundle\Entity\Competition;
 use ApiBundle\Entity\Player;
 use ApiBundle\Form\PlayerType;
 
-class PlayersController extends FOSRestController
-{
+class PlayersController extends FOSRestController {
+	
   public function sendResponseWithETag(Request $request,\FOS\RestBundle\View\View $response) {
 	 
-	  $content = $this->handleView($response);
-	  $response->getResponse()->setEtag(md5($content));
-	  $response->getResponse()->setPublic();
+	$content = $this->handleView($response);
+	$response->getResponse()->setEtag(md5($content));
+	$response->getResponse()->setPublic();
 	  
-	  /** compare the 'if-not-match' from the request to the etag
+	/** compare the 'if-not-match' from the request to the etag
 		  send 304 not modified if match **/   
-	  if (!$response->getResponse()->isNotModified($request))
-	  		return $content;
-	  else	
-	  		return $this->view('',Response::HTTP_NOT_MODIFIED);
-	  
+	if (!$response->getResponse()->isNotModified($request))
+	    return $content;
+	else	
+	    return $this->view('',Response::HTTP_NOT_MODIFIED);
   }
   
   
@@ -48,16 +47,16 @@ class PlayersController extends FOSRestController
 
   public function getTeamsAction(Request $request) {
 	         
-	    $repository = $this->getDoctrine()
-	        			   ->getManager()
-	        			   ->getRepository('ApiBundle:Team');
-	        				  
-	    $teams = $repository->findAll();				       
-	    $response = $this->view($teams);
-	    
-	    return $this->sendResponseWithETag($request,$response);
-	   
-    }
+        $repository = $this->getDoctrine()
+		       ->getManager()
+		       ->getRepository('ApiBundle:Team');
+
+        $teams = $repository->findAll();				       
+        $response = $this->view($teams);
+
+        return $this->sendResponseWithETag($request,$response);
+
+  }
     
   /**
   * @View()
@@ -67,7 +66,7 @@ class PlayersController extends FOSRestController
     
   public function getPlayersByTeamAction(Team  $team,Request $request) {
 	    
-	    $response = $this->view($team->getPlayers());
+	$response = $this->view($team->getPlayers());
         return $this->sendResponseWithETag($request,$response);
   }  
        
@@ -78,12 +77,14 @@ class PlayersController extends FOSRestController
 
   public function getCompetitionsAction(Player $player,Request $request) {
 	    
-	    $competitions = $player->getCompetitions();
-	    $array = array();
-	    foreach($player->getCompetitions() as $compo)
-	    	{$array[] = array('competition' => $compo->getCompetition(),'goals' => $compo->getGoals(),'assist' => $compo->getAssists())	;
-		    }
-	    $response = $this->view($array);
+	$competitions = $player->getCompetitions();
+	$array = array();
+	
+	foreach($player->getCompetitions() as $compo) {
+	    $array[] = array('competition' => $compo->getCompetition(),'goals' => $compo->getGoals(),'assist' => $compo->getAssists())	;
+	}
+	
+	$response = $this->view($array);
         return $this->sendResponseWithETag($request,$response);
 	    
   }
@@ -95,9 +96,8 @@ class PlayersController extends FOSRestController
   
   public function postPlayerAction(Request $request) {
 	    
-	    $player = new Player;
-	    
-		$form = $this->get('form.factory')->createNamed('',PlayerType::class, $player,array('method' => 'POST'));
+	$player = new Player;
+	$form = $this->get('form.factory')->createNamed('',PlayerType::class, $player,array('method' => 'POST'));
         $form->handleRequest($request);
  
         if ($form->isValid()) {
@@ -107,18 +107,18 @@ class PlayersController extends FOSRestController
             $em->flush();
             $status_code = 200;
             $message = array("status"=> "valid",
-							 "status_code"=> 200,
-						     "message" => $player->getName()." ".$player->getLastName()." has been created");
+			     "status_code"=> 200,
+			     "message" => $player->getName()." ".$player->getLastName()." has been created"
+			    );
         }
         else {
-	        $status_code = 400;	 
-	        $message = array("status"=> "bad request",
-							 "status_code"=> 400,
-							 "message" => "some parameters are wrong");
- 		}
+	    $status_code = 400;	 
+	    $message = array("status"=> "bad request",
+			     "status_code"=> 400,
+		             "message" => "some parameters are wrong");
+ 	}
         
         $response = $this->view($message,$status_code);
-       
         return $this->sendResponseWithETag($request,$response);
 	
         
@@ -141,16 +141,15 @@ class PlayersController extends FOSRestController
             $em->flush();
             $status_code = 200;
             $message = array("status"=> "valid",
-							 "status_code"=> 200,
-							 "message" => $player->getName()." ".$player->getLastName()." has been updated");
-
+			     "status_code"=> 200,
+			     "message" => $player->getName()." ".$player->getLastName()." has been updated"
+			    );
         }
-
         else {
-	          $status_code = 400;	 
-	          $message = array("status"=> "bad request",
-								"status_code"=> 400,
-							    "message" => "some parameters are wrong");
+	    $status_code = 400;	 
+	    $message = array("status"=> "bad request",
+			     "status_code"=> 400,
+			     "message" => "some parameters are wrong");
 	        	 
         }
 
@@ -164,22 +163,20 @@ class PlayersController extends FOSRestController
   * @Delete("/players/{player}", requirements={"player" = "\d+"})
   */
 
-    public function deletePlayerAction(Player $player,Request $request) {
+  public function deletePlayerAction(Player $player,Request $request) {
 
-	    $em = $this->getDoctrine()->getManager();
+	$em = $this->getDoctrine()->getManager();
         $em->remove($player);
         $em->flush();
-        
-        
-        $message = array("status"=> "valid",
-						 "status_code"=> 200,
-						 "message" => $player->getName()." ".$player->getLastName()." has been removed");
+          
+	$message = array("status"=> "valid",
+			 "status_code"=> 200,
+			 "message" => $player->getName()." ".$player->getLastName()." has been removed"
+			);
 	    
-	    $response = $this->view($message);
-
-	    return $this->sendResponseWithETag($request,$response);
-	    
-    }
+	$response = $this->view($message);
+        return $this->sendResponseWithETag($request,$response);
+  }
 
   /**
   * @View()
@@ -188,22 +185,20 @@ class PlayersController extends FOSRestController
  
   public function assignTeamAction(Team $team,Player $player,Request $request) {
 
-	    $player->setTeam($team);
-	 
-	    $em = $this->getDoctrine()->getManager();
+	$player->setTeam($team);
+	$em = $this->getDoctrine()->getManager();
         $em->persist($player);
         $em->flush();
         
-        
         $message = array("status"=> "valid",
-						 "status_code"=> 200,
-						 "message" => $player->getName()." ".$player->getLastName()." has been assigned to ".$team->getName());
+		         "status_code"=> 200,
+			 "message" => $player->getName()." ".$player->getLastName()." has been assigned to ".$team->getName()
+			);
 	    
-	    $response = $this->view($message);
+	$response = $this->view($message);
+        return $this->sendResponseWithETag($request,$response);
 
-	    return $this->sendResponseWithETag($request,$response);
-
-    }
+  }
 
   /**
   * @View()
@@ -212,24 +207,25 @@ class PlayersController extends FOSRestController
   
   public function getCompetitionPlayersAction(Competition $competition,Request $request) {
 	    
-	    $players=array();
+	$players=array();
     	foreach($competition->getPlayers() as  $player) {
-		    $players[] = $player->getPlayer();
-		}
+	    $players[] = $player->getPlayer();
+	}
 	    
-	    if (empty($players)) {
-		    $message = array("status"=> "valid",
-								 "status_code"=> 204,
-								 "message" => "No Content");
+	if (empty($players)) {
+	    $message = array("status"=> "valid",
+		             "status_code"=> 204,
+			     "message" => "No Content"
+			    );
 	    
-			$response = $this->view($message,204);
-		}
-		else
-	    	$response = $this->view($players);
+	    $response = $this->view($message,204);
+	}
+	else
+	    $response = $this->view($players);
         
         return $this->sendResponseWithETag($request,$response);
 	    
-    }
+  }
   /**
   * @View()
   * @Get("/competitions/{competition}/teams", requirements={"competition" = "\d+"})
@@ -237,19 +233,20 @@ class PlayersController extends FOSRestController
   
   public function getTeamsPlayerssAction(Competition $competition,Request $request) {
 	    
-	    $em = $this->getDoctrine()->getManager();
-		$teams = $em->getRepository('ApiBundle:Team')
-					->findTeamByCompetitionPlayers($competition);
+	$em = $this->getDoctrine()->getManager();
+	$teams = $em->getRepository('ApiBundle:Team')
+		    ->findTeamByCompetitionPlayers($competition);
 	    
-	   if (empty($teams)) {
-		    	$message = array("status"=> "valid",
-								 "status_code"=> 204,
-								 "message" => "No Content");
+	if (empty($teams)) {
+	    $message = array("status"=> "valid",
+		             "status_code"=> 204,
+			     "message" => "No Content"
+			    );
 	    
-				$response = $this->view($message,204);
-		}
-		else
-	    	$response = $this->view($teams);
+	    $response = $this->view($message,204);
+	}
+	else
+	    $response = $this->view($teams);
         return $this->sendResponseWithETag($request,$response);
 	    
     }
@@ -294,41 +291,42 @@ class PlayersController extends FOSRestController
     }
   
     /**
-	* @QueryParam(name="name",requirements="[a-z]+", nullable=true)
-	* @QueryParam(name="lastname",requirements="[a-z]+", nullable=true)
-	* @QueryParam(name="height",requirements="\d+", nullable=true)
-	* @QueryParam(name="weight",requirements="\d+", nullable=true)
-	* @QueryParam(name="country",requirements="[a-z]+", nullable=true)
-	* @QueryParam(name="age", requirements="\d+", nullable=true)
-	* @QueryParam(name="rightFoot", requirements="\d+", nullable=true)
-	* @QueryParam(name="height",requirements="\d+", nullable=true)
-    * @param ParamFetcher $paramFetcher
-    * @Get("/teams/{team}/players", requirements={"team" = "\d+"})
-    */
+     * @QueryParam(name="name",requirements="[a-z]+", nullable=true)
+     * @QueryParam(name="lastname",requirements="[a-z]+", nullable=true)
+     * @QueryParam(name="height",requirements="\d+", nullable=true)
+     * @QueryParam(name="weight",requirements="\d+", nullable=true)
+     * @QueryParam(name="country",requirements="[a-z]+", nullable=true)
+     * @QueryParam(name="age", requirements="\d+", nullable=true)
+     * @QueryParam(name="rightFoot", requirements="\d+", nullable=true)
+     * @QueryParam(name="height",requirements="\d+", nullable=true)
+     * @param ParamFetcher $paramFetcher
+     * @Get("/teams/{team}/players", requirements={"team" = "\d+"})
+     */
     
     public function getTeamPlayersAction(Team $team,ParamFetcher $paramFetcher,Request $request) {
 	    
 	    
-	   $players = $this->getDoctrine()
-	        		   ->getManager()
-	        		   ->getRepository('ApiBundle:Player');
+	  $players = $this->getDoctrine()
+	  	          ->getManager()
+	        	  ->getRepository('ApiBundle:Player');
 	        				   
-	   $filters = array_filter($paramFetcher->all(),function($value) { return $value !== null; });
-	   $filters['team'] = $team->getId();
-	   $players = $players->findBy($filters);
+	  $filters = array_filter($paramFetcher->all(),function($value) { return $value !== null; });
+	  $filters['team'] = $team->getId();
+	  $players = $players->findBy($filters);
 	   
-	   if (empty($players)) {
-		    $message = array("status"=> "valid",
-								 "status_code"=> 204,
-								 "message" => "No Content");
+	  if (empty($players)) {
+	      $message = array("status"=> "valid",
+			       "status_code"=> 204,
+			       "message" => "No Content"
+			      );
 	    
-			$response = $this->view($message,204);
-		}
-		else
-	    	$response = $this->view($players);
+	      $response = $this->view($message,204);
+	  }
+	  else
+	      $response = $this->view($players);
 	    
 	    
-        return $this->sendResponseWithETag($request,$response);
+         return $this->sendResponseWithETag($request,$response);
 	  
     }
     
